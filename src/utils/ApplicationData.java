36d03a694423be2c105f0acbf024f61d740c7afd
MikolajChojnacki <mikolaj.chojnacki@wp.pl>
2018-04-03 21:01:27 +0200

    Poprawienie dolaczenia biblioteki j connector

--- src/utils/ApplicationData.java
+++ src/utils/ApplicationData.java
@@ -5,7 +5,7 @@ public class ApplicationData {
 	private static ApplicationData instance = null;
 
 	private final static String DRIVER = "com.mysql.jdbc.Driver";
-	private final static String DB_PATH = "jdbc:mysql://localhost:3307//zamowienia";
+	private final static String DB_PATH = "jdbc:mysql://localhost:3307/plants";
 	private final static String USER = "root";
 	private final static String PASSWORD = "";
 
